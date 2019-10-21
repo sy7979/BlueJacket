2a0647b7a7b24aae5a20cdf23862f08a467261de
sy7979 <gkgkghgh9595@gmail.com>
2019-10-21 10:24:13 +0900

    login 완성

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login, logout as auth_logout
 
 # Create your views here.
 
@@ -11,6 +12,19 @@ def signup(request):
             return redirect('posts:index')
     else:
         form = UserCreationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/form.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('posts:index')
+    else:
+        form = AuthenticationForm()
     context = {
         'form': form
     }
--- board/urls.py
+++ board/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from posts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('posts/', include('posts.urls')),
+    path('', views.index)
 ]
